378c3445fce5db3eb7af357b80ffbf2b9812fc1e
MayBan <cocooked@gmail.com>
2019-10-18 09:04:07 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ProgressBarsDynamically
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lv = new System.Windows.Forms.ListView();
+            this.Item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Completion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnCreateList = new System.Windows.Forms.Button();
+            this.btnChangeListValue = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 28);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Create";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // lv
+            // 
+            this.lv.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Item,
+            this.Value,
+            this.Completion});
+            this.lv.HideSelection = false;
+            this.lv.Location = new System.Drawing.Point(123, 296);
+            this.lv.Name = "lv";
+            this.lv.Size = new System.Drawing.Size(470, 186);
+            this.lv.TabIndex = 3;
+            this.lv.UseCompatibleStateImageBehavior = false;
+            this.lv.View = System.Windows.Forms.View.Details;
+            // 
+            // Item
+            // 
+            this.Item.Text = "Item";
+            this.Item.Width = 108;
+            // 
+            // Value
+            // 
+            this.Value.Text = "Value";
+            this.Value.Width = 145;
+            // 
+            // Completion
+            // 
+            this.Completion.Text = "Completion";
+            this.Completion.Width = 177;
+            // 
+            // btnCreateList
+            // 
+            this.btnCreateList.Location = new System.Drawing.Point(27, 296);
+            this.btnCreateList.Name = "btnCreateList";
+            this.btnCreateList.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateList.TabIndex = 4;
+            this.btnCreateList.Text = "Run List";
+            this.btnCreateList.UseVisualStyleBackColor = true;
+            this.btnCreateList.Click += new System.EventHandler(this.btnCreateList_Click);
+            // 
+            // btnChangeListValue
+            // 
+            this.btnChangeListValue.Enabled = false;
+            this.btnChangeListValue.Location = new System.Drawing.Point(27, 325);
+            this.btnChangeListValue.Name = "btnChangeListValue";
+            this.btnChangeListValue.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeListValue.TabIndex = 5;
+            this.btnChangeListValue.Text = "Random";
+            this.btnChangeListValue.UseVisualStyleBackColor = true;
+            this.btnChangeListValue.Click += new System.EventHandler(this.btnChangeListValue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 494);
+            this.Controls.Add(this.btnChangeListValue);
+            this.Controls.Add(this.btnCreateList);
+            this.Controls.Add(this.lv);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.ListView lv;
+        private System.Windows.Forms.ColumnHeader Item;
+        private System.Windows.Forms.ColumnHeader Value;
+        private System.Windows.Forms.ColumnHeader Completion;
+        private System.Windows.Forms.Button btnCreateList;
+        private System.Windows.Forms.Button btnChangeListValue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//https://stackoverflow.com/questions/39181805/accessing-progressbar-in-listview
+namespace ProgressBarsDynamically
+{
+    public partial class Form1 : Form
+    {
+        const int processbarSizeHeight = 25;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+        private List<ProgressBar> bars = new List<ProgressBar>();
+        private ProgressBar CurrentBar;
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+                this.CreateNewProcessBar(i.ToString(), i);
+            }
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private void CreateNewProcessBar(string name,int index)
+        {
+            ProgressBar item = new ProgressBar();
+            item.Location = new System.Drawing.Point(100, index* processbarSizeHeight + index*10 + 10);
+            item.Name = String.Format("processBar_{0}",name);
+            item.Size = new System.Drawing.Size(400, processbarSizeHeight);
+            item.TabIndex = 1;
+          
+            this.BeginInvoke(new Action(() =>
+            {
+                this.Controls.Add(item);
+              
+            }));
+            this.bars.Add(item);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+
+        }
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (ProgressBar bar in bars)
+            {
+                CurrentBar = bar;
+                bar.Value = 0;
+                for (int i = 1; i <= 100; i++)
+                {
+                    Thread.Sleep(10);
+                    backgroundWorker1.ReportProgress(i);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            CurrentBar.Value = e.ProgressPercentage;
+        }
+
+        private void AddLVItem(string key, string name, int value)
+        {
+            ListViewItem lvi = new ListViewItem();
+            ProgressBar pb = new ProgressBar();
+
+            lvi.SubItems[0].Text = name;
+            lvi.SubItems.Add(value.ToString());
+            lvi.SubItems.Add("");
+            lvi.SubItems.Add(key);            // LV has 3 cols; this wont show
+            lv.Items.Add(lvi);
+
+            Rectangle r = lvi.SubItems[2].Bounds;
+            pb.SetBounds(r.X, r.Y, r.Width, r.Height);
+            pb.Minimum = 1;
+            pb.Maximum = 100;
+            pb.Value = value;
+            pb.Name = key;                   // use the key as the name
+            lv.Controls.Add(pb);
+        }
+        private void UpdateItemValue(string key, int value)
+        {
+            ListViewItem lvi;
+            ProgressBar pb;
+
+            // find the LVI based on the "key" in 
+            lvi = lv.Items.Cast<ListViewItem>().FirstOrDefault(q => q.SubItems[3].Text == key);
+            if (lvi != null)
+                lvi.SubItems[1].Text = value.ToString();
+
+            pb = lv.Controls.OfType<ProgressBar>().FirstOrDefault(q => q.Name == key);
+            if (pb != null)
+                pb.Value = value;
+        }
+
+        private void btnCreateList_Click(object sender, EventArgs e)
+        {
+            AddLVItem("A", "Worker Zig", 1);
+            AddLVItem("B", "Jame Test", 1);
+            AddLVItem("C", "Rob Task", 1);
+            AddLVItem("D", "Kate HR", 1);
+            this.btnCreateList.Enabled = false;
+            this.btnChangeListValue.Enabled = true;
+        }
+
+        private void btnChangeListValue_Click(object sender, EventArgs e)
+        {
+            Random randNum = new Random();
+
+            UpdateItemValue("A", randNum.Next(1, 100));
+            UpdateItemValue("B", randNum.Next(1, 100));
+            UpdateItemValue("C", randNum.Next(1, 100));
+            UpdateItemValue("D", randNum.Next(1, 100));
+        }
+    }
+}
